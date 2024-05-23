02384f15cd1bc75fd41ce68ef3eed31ffc1b928b
agordon06 <150299686+agordon06@users.noreply.github.com>
2024-05-23 00:58:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = {
+        html: 'advanced',
+        css: 'advanced',
+        js: 'advanced',
+        php: 'intermediate',
+        sql: 'intermediate'
+    };
+
+    return skills;
+}
